20d86029c2b1767e1f1d907a5268bad8171ad11b
Shivam Rathore <shivam.rathore010@gmail.com>
2019-10-28 21:19:15 +0530

    core: net/http converted to valyala/fasthttp

--- main.go
+++ main.go
@@ -2,9 +2,36 @@ package main
 
 import (
 	"fmt"
-	"net/http"
+	"log"
+
+	"os"
+
+	"github.com/valyala/fasthttp"
 )
 
 func main() {
-	fmt.Println(http.ListenAndServe(":80", http.FileServer(http.Dir("."))))
+
+	dir, _ := os.Getwd()
+	fs := &fasthttp.FS{
+		Root:               dir,
+		AcceptByteRange:    true,
+		GenerateIndexPages: true,
+	}
+	fsHandler := fs.NewRequestHandler()
+
+	requestHandler := func(ctx *fasthttp.RequestCtx) {
+		fsHandler(ctx)
+	}
+
+	fmt.Println("Starting HTTP server on PORT 80")
+	go func() {
+		if err := fasthttp.ListenAndServe(":80", requestHandler); err != nil {
+			log.Fatalln("error in ListenAndServe: " + err.Error())
+		}
+	}()
+
+	fmt.Printf("Serving files from directory: '%s'\n", dir)
+
+	// Wait forever.
+	select {}
 }
